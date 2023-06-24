e562d7357ad85ec4c901e805e55a07351181309f
hanan3889 <rebaia_hanan@yahoo.fr>
2023-06-24 13:50:12 +0200

    ✨add API weather in project✨

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,24 @@
   import loader from './assets/loader.svg'
   import './App.css'
+  import { useEffect, useState } from 'react';
+  const APIKEY = import.meta.env.VITE_WEATHER_API_KEY
   
   function App() {
+    const [weatherData, setWeatherData] = useState(null)
+
+    useEffect(() => {
+
+      fetch(`http://api.airvisual.com/v2/nearest_city?key=${APIKEY}`)
+      .then(response =>{
+        console.log(response);
+        return response.json()
+      })
+      .then(data => {
+
+        console.log(data)
+      })
+
+    }, [])
   
     return (
 
